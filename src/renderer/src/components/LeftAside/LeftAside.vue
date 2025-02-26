e900243656456409f3d382a51330ab9c8d0bdfe0
yingyu <40139131+LiangKuan4451@users.noreply.github.com>
2025-02-26 16:02:35 +0800

    排序功能

--- src/renderer/src/components/LeftAside/LeftAside.vue
+++ src/renderer/src/components/LeftAside/LeftAside.vue
@@ -13,7 +13,7 @@
           :controls="false"
           class=""
           data-type="1"
-          @change="$emit('pageChange', pdfCurrentPage)"
+          @change="callScrollpage(pdfCurrentPage)"
         >
           <template #suffix>
             <span>页 / {{ pdfTotalPages }} 页</span>
@@ -31,7 +31,7 @@
           :precision="0"
           :controls="false"
           data-type="2"
-          @change="$emit('scaleChange', pdfCurrentPage)"
+          @change="callRenderAllPages"
         >
           <template #suffix>
             <span>%</span>
@@ -41,8 +41,13 @@
     </el-row>
   </div>
   <!-- 渲染所有缩略图 -->
-  <el-scrollbar id="pdfPageList" ref="pdfPageList" max-height="100vh">
-    <div v-for="(thumbnail, index) in pdfThumbnails" :key="index" class="relative" @click="$emit('thumbnailClick', (index+1))">
+  <el-scrollbar id="pdfPageList" ref="pdfPageList" height="100vh">
+    <div
+      v-for="(thumbnail, index) in pdfThumbnails"
+      :key="index"
+      class="relative"
+      @click="callScrollpage(index + 1)"
+    >
       <img class="mx-auto" :src="thumbnail" :alt="'Page ' + (index + 1)" />
       <span class="absolute top-0 w-6 h-6 text-center">{{ index + 1 }}</span>
     </div>
@@ -56,15 +61,26 @@ const pdfSetting = inject('pdfSetting')
 const pdfCurrentPage = inject('pdfCurrentPage')
 const pdfTotalPages = inject('pdfTotalPages')
 const pdfThumbnails = inject('pdfThumbnails')
+const scrollpage = inject('scrollpage')
+const renderAllPages = inject('renderAllPages')
+const callRenderAllPages = () => {
+  renderAllPages()
+}
+const callScrollpage = (page) => {
+  scrollpage(page)
+}
 </script>
 
 <style scoped>
 .numPages {
   border-bottom: 1px solid var(--el-color-success);
-  @apply py-2 px-3;
+  @apply py-3 px-3;
 }
 #pdfPageList {
   @apply p-1 h-full;
+  div:last-child {
+    @apply mb-12;
+  }
   div {
     @apply py-5 cursor-pointer ease-in-out transition-all duration-100;
     @apply hover:bg-lime-300 hover:bg-opacity-50;
@@ -76,9 +92,6 @@ const pdfThumbnails = inject('pdfThumbnails')
     img {
       border: 1px solid var(--el-color-success);
     }
-  }
-  div:last-of-type {
-    @apply mb-28;
   }
 }
 
--- src/renderer/src/components/RightAside/RightAside.vue
+++ src/renderer/src/components/RightAside/RightAside.vue
@@ -1,28 +1,91 @@
 <template>
-  <el-collapse :accordion="false" >
+  <el-collapse :accordion="false">
     <el-collapse-item
-      v-for="(item, index) in pdfPaperContentIndexData"
+      v-for="(item, index) in pdfIndexData.indexData"
       :key="index"
-      :title="item.name"
+      :title="item.title"
       :name="item.name"
-      class="px-2"
     >
       <!-- content -->
-      <div v-for="(data, index) in item.data" :key="index">
-        {{ data.content }} - {{ data.pdfPage }}
-      </div>
+      <el-table
+        ref="pdfIndexTable"
+        :data="item.data"
+        stripe
+        style="width: 100%"
+        size="small"
+        @sort-change="updateTable(item[index], index)"
+      >
+        <el-table-column prop="content" sortable :sort-method="sortText" label="文本" />
+        <el-table-column prop="pdfPage" label="页码" />
+        <!-- <el-table-column label="高亮">
+          <template #default="scope">
+            <el-checkbox v-model="item.highlight">{{ item.highlight }}</el-checkbox>
+          </template>
+        </el-table-column> -->
+        <el-table-column fixed="right" label="选项">
+          <template #default="scope">
+            <el-button-group>
+              <el-button link type="primary" size="small" @click.prevent="edit(scope.$index)">
+                编辑
+              </el-button>
+              <el-button link type="danger" size="small" @click.prevent="deleteRow(index, scope)">
+                删除
+              </el-button>
+            </el-button-group>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-collapse-item>
   </el-collapse>
 </template>
+.store
+<script setup>
+import { inject, watch } from 'vue'
 
-<script setup >
-import { inject } from 'vue'
+const pdfIndexData = inject('pdfIndexData')
+const removeTextHightLight = inject('removeTextHightLight')
+const sortText = inject('sortText')
+const pdfIndexTable = inject('pdfIndexTable')
+const pdfsetting = inject('pdfSetting')
 
-const pdfPaperContentIndexData = inject('pdfPaperContentIndexData')
+const updateTable = (tableData, index) => {
+  // console.log(Object.keys(pdfIndexData.value))
+  // console.log('tableData', tableData, ref)
+
+  // console.log(pdfIndexTable.value[0])
+  // console.log('当前表格索引:', index)
+  // console.log('当前表格数据:', tableData)
+  // console.log('当前表格index:', index)
+
+  // 通过 ref 获取表格实例
+  const tableInstance = pdfIndexTable.value[index]
+  if (tableInstance) {
+    console.log('表格实例:', tableInstance)
+    // 获取当前表格的选中行
+    const selectedRows = tableInstance.getSelectionRows()
+    // tableInstance.toggleRowSelection(selectedRows, true)
+    console.log('选中行:', selectedRows)
+  }
+}
+
+const callRemoveTextHightLight = (content) => {
+  removeTextHightLight(content)
+}
+
+const deleteRow = (dataName, index) => {
+  console.log('dataName', dataName)
+  console.log('index.store', index.store)
+  console.log('index.row', index.row)
+  console.log('index.$index', index.$index)
+
+  // callRemoveTextHightLight(pdfIndexData.value[dataName].data[index].content)
+  // pdfIndexData.value[dataName].data.splice(index, 1)
+}
 </script>
 
 <style scoped>
-.el-collapse-item{
+.el-collapse-item {
   border-bottom: 1px solid var(--el-color-success) !important;
+  padding: 0.5px 0.75rem;
 }
 </style>
